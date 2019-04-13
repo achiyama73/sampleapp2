09b299c6aa0a7326a07e4de616a1f59deebec488
achiyama73 <kohei.akiyama73@gmail.com>
2019-04-13 19:03:51 +0900

    finish ch09

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,11 @@
 module ApplicationHelper
+  # ページごとの完全なタイトルを返します
   def full_title(page_title = '')
     base_title = "Ruby on Rails Tutorial Sample App"
     if page_title.empty?
-      return base_title
+      base_title
     else
-      return page_title + " | " + base_title
+      page_title + " | " + base_title
     end
   end
 end
