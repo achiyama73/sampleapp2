6ce16d4f3915cddd980978b64b16359eeb1d4dad
achiyama73 <kohei.akiyama73@gmail.com>
2019-05-01 17:33:53 +0900

    Add account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
   # ユーザー一覧ページ
   def index
-    @users = User.paginate(page: params[:page])
+    @users = User.where(activated: true).paginate(page: params[:page])
   end
 
   # 新規登録ページ
@@ -16,6 +16,7 @@ class UsersController < ApplicationController
   # ユーザーの詳細ページ
   def show
     @user = User.find(params[:id])
+    redirect_to root_url and return unless @user.activated? 
   end
 
   # ユーザー作成
@@ -23,7 +24,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      UserMailer.account_activation(@user).deliver_now
+      @user.send_activation_email
       flash[:info] = "Please check your email to activate your account."
       redirect_to root_url
     else
